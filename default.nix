3c79e91af7052f3f214008f74d57f80a3b7fd6e8
Ben Gamari <ben@smart-cactus.org>
2018-06-22 16:54:18 -0400

    nix: passthru ukb

--- default.nix
+++ default.nix
@@ -8,3 +8,4 @@ in
   appendConfigureFlag
     (appendConfigureFlag drv "--ghc-option=-DPOS_TAG_PATH=\"${./pos-tag.py}\"")
     "--ghc-option=-DUKB_WSD_PATH=\"${ukb}/bin/ukb_wsd\""
+    // { passthru.ukb = ukb; }
