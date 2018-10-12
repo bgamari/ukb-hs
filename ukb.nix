4605b1d1b8acd2232b58bbe9fd970a0437432795
Ben Gamari <ben@smart-cactus.org>
2018-10-11 20:48:44 -0400

    nix: Add sha256 for ukb tarball

--- ukb.nix
+++ ukb.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
     owner = "bgamari";
     repo = "ukb";
     rev = "f4243169a7500bfcded4ff472ac71e3428a2f7ab";
-    sha256 = null;
+    sha256 = "0fky7gmy4k1gdgb60kr6a8rvlz0c60qmp0n1srasxz34h2lfrnjk";
   };
   sourceRoot = "source/src";
   patches = [ ./ukb-parsability.patch ];
